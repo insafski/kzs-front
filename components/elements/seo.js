7ce8370bf78a30d5c446fa25e79ef473c863ba09
Dmitriy Konovalov <dimusig@gmail.com>
2021-03-04 02:12:59 +0300

    feat: common page component

--- components/elements/seo.js
+++ components/elements/seo.js
@@ -1,49 +1,50 @@
+import React from "react";
 import { NextSeo } from "next-seo";
 import PropTypes from "prop-types";
 import { getStrapiMedia } from "utils/media";
 import { mediaPropTypes } from "utils/types";
 
-const Seo = ({ metadata }) => {
-  // Prevent errors if no metadata was set
-  if (!metadata) return null;
+export default function Seo({ metadata }) {
+	// Prevent errors if no metadata was set
+	if (!metadata) {
+		return null;
+	}
 
-  return (
-    <NextSeo
-      title={metadata.metaTitle}
-      description={metadata.metaDescription}
-      openGraph={{
-        // Title and description are mandatory
-        title: metadata.metaTitle,
-        description: metadata.metaDescription,
-        // Only include OG image if we have it
-        // Careful: if you disable image optimization in Strapi, this will break
-        ...(metadata.shareImage && {
-          images: Object.values(metadata.shareImage.formats).map((image) => {
-            return {
-              url: getStrapiMedia(image.url),
-              width: image.width,
-              height: image.height,
-            };
-          }),
-        }),
-      }}
-      // Only included Twitter data if we have it
-      twitter={{
-        ...(metadata.twitterCardType && { cardType: metadata.twitterCardType }),
-        ...(metadata.twitterUsername && { cardType: metadata.twitterUsername }),
-      }}
-    />
-  );
-};
+	return (
+		<NextSeo
+			title={metadata.metaTitle}
+			description={metadata.metaDescription}
+			openGraph={{
+				// Title and description are mandatory
+				title: metadata.metaTitle,
+				description: metadata.metaDescription,
+				// Only include OG image if we have it
+				// Careful: if you disable image optimization in Strapi, this will break
+				...metadata.shareImage && {
+					images: Object.values(metadata.shareImage.formats).map(image => {
+						return {
+							url: getStrapiMedia(image.url),
+							width: image.width,
+							height: image.height,
+						};
+					}),
+				},
+			}}
+			// Only included Twitter data if we have it
+			twitter={{
+				...metadata.twitterCardType && { cardType: metadata.twitterCardType },
+				...metadata.twitterUsername && { cardType: metadata.twitterUsername },
+			}}
+		/>
+	);
+}
 
 Seo.propTypes = {
-  metadata: PropTypes.shape({
-    metaTitle: PropTypes.string.isRequired,
-    metaDescription: PropTypes.string.isRequired,
-    shareImage: mediaPropTypes,
-    twitterCardType: PropTypes.string,
-    twitterUsername: PropTypes.string,
-  }),
+	metadata: PropTypes.shape({
+		metaTitle: PropTypes.string.isRequired,
+		metaDescription: PropTypes.string.isRequired,
+		shareImage: mediaPropTypes,
+		twitterCardType: PropTypes.string,
+		twitterUsername: PropTypes.string,
+	}),
 };
-
-export default Seo;
--- pages/index.js
+++ pages/index.js
@@ -1,33 +1,16 @@
+import React from "react";
 import { gql } from "@apollo/client";
 import get from "lodash/get";
 
 import Page from "../components/containers/Page";
+
 import { client } from "./api/apollo";
-
-export async function getStaticPaths() {
-	const result = await client.query({
-		query: gql`
-			query Pages {
-				pages {
-					slug
-				}
-			}
-		`,
-	});
-
-	const paths = get(result, "data.pages", []).map(({ slug }) => ({ params: { slug } }));
-
-	return {
-		paths,
-		fallback: false,
-	};
-}
 
 export async function getStaticProps({ params, preview = null }) {
 	const result = await client.query({
 		query: gql`
-			query Page($slug: String!) {
-				pages(where: {slug: {_eq: $slug}, _not: {slug: {_eq: "/"}}}) {
+			query Page {
+				pages(where: {slug: {_eq: "/"}}) {
 					id
 					createdAt
 					deletedAt
