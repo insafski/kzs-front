8acd8b5ba0ab1f70f6c65eb051b7887509e5674a
Dmitriy Konovalov <dimusig@gmail.com>
2021-03-05 00:50:06 +0300

    feat: tailwind forms

--- components/Layout/index.js
+++ components/Layout/index.js
@@ -46,7 +46,7 @@ export default function Layout({ children, global }) {
 					<div className={"header-middle pt-4"}>
 						<div className={"container mx-auto"}>
 							<button>Каталог</button>
-							<input type="text"/>
+							<input type="text" className="mt-1 block w-full rounded-md border-gray-300 shadow-sm focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50" />
 						</div>
 					</div>
 				}
