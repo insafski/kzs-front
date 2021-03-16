a3b55cc6768f5fbcb95f9ee639493f4d1058790a
Insaf Almukhametov <insaf_13@mail.ru>
2021-03-17 01:43:31 +0300

    fix: show manufacturers block

--- components/elements/ListElement/Manufacturer/Manufacturer.jsx
+++ components/elements/ListElement/Manufacturer/Manufacturer.jsx
@@ -1,16 +1,21 @@
 import React from "react";
+import PropTypes from "prop-types";
 
-export default function Manufacturer({ title, url }) {
+export default function Manufacturer({ heading, slug, picture }) {
 	return (
 		<div className={"manufacturer md:px-2 md:w-2/4 lg:w-2/6 xl:w-3/12"}>
-			<a
-				href={`/catalog/${url}`}
-			>
+			<a href={`/catalog/${slug}`}>
 				<div className={"flex justify-between items-center text-xl p-8 bg-gray-100 mb-4 rounded-md"}>
-					{title}
+					{heading.title}
 					<img src="https://nationalrent.ru/local/templates/nationalrent/img/catalog/doosan-logo.png" alt="" style={{ height: 40 }}/>
 				</div>
 			</a>
 		</div>
 	);
 }
+
+Manufacturer.propTypes = {
+	heading: PropTypes.object,
+	slug: PropTypes.string,
+	picture: PropTypes.object,
+};
