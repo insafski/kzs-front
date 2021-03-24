d35f1d029ed780c13c9b6c91da6b8e76f7954b1d
Insaf Almukhametov <insaf_13@mail.ru>
2021-03-24 23:13:58 +0300

    feat: add  markdown-remark

--- components/sections/Text/Text.jsx
+++ components/sections/Text/Text.jsx
@@ -1,13 +1,23 @@
 import React from "react";
+import ReactMarkdown from "react-markdown";
 import PropTypes from "prop-types";
+import cx from "classnames";
 
 import Block from "@/components/containers/Block";
+
+import styles from "./Text.module.scss";
 
 export default function Text({ heading, text }) {
 	return (
 		<Block heading={heading} className={"text"}>
-			<div className={"container"}>
-				{text}
+			<div className={cx("container", styles.markdown)}>
+				{
+					!!text && (
+						<ReactMarkdown allowDangerousHtml>
+							{text}
+						</ReactMarkdown>
+					)
+				}
 			</div>
 		</Block>
 	);
