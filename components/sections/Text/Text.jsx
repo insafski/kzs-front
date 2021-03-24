dda4f69ff58f28c4de9ddd0a5c42022d5ad70f79
Insaf Almukhametov <insaf_13@mail.ru>
2021-03-25 00:37:26 +0300

    feat: more price-table

--- components/sections/Text/Text.jsx
+++ components/sections/Text/Text.jsx
@@ -219,6 +219,412 @@ function TablePrice() {
 					<td className={"border"}>7</td>
 					<td className={"border"}>10500</td>
 				</tr>
+				<tr>
+					<td className={"border p-3 font-bold"} colSpan={5}>КПП (спаренная  с ГТР)</td>
+				</tr>
+				<tr>
+					<td className={"border p-4"}>Диагностика КПП, ГТР</td>
+					<td className={"border"}>4</td>
+					<td className={"border"}>6000</td>
+					<td className={"border"}>4</td>
+					<td className={"border"}>6000</td>
+				</tr>
+				<tr>
+					<td className={"border p-4"}>Демонтаж КПП, ГТР</td>
+					<td className={"border"}>16</td>
+					<td className={"border"}>24000</td>
+					<td className={"border"}>16</td>
+					<td className={"border"}>24000</td>
+				</tr>
+				<tr>
+					<td className={"border p-4"}>Разборка, мойка и дефектовка КПП,ГТР</td>
+					<td className={"border"}>12</td>
+					<td className={"border"}>18000</td>
+					<td className={"border"}>14</td>
+					<td className={"border"}>21000</td>
+				</tr>
+				<tr>
+					<td className={"border p-4"}>Сборка КПП,ГТР</td>
+					<td className={"border"}>22</td>
+					<td className={"border"}>33000</td>
+					<td className={"border"}>24</td>
+					<td className={"border"}>36000</td>
+				</tr>
+				<tr>
+					<td className={"border p-4"}>Монтаж КПП,ГТР</td>
+					<td className={"border"}>16</td>
+					<td className={"border"}>24000</td>
+					<td className={"border"}>16</td>
+					<td className={"border"}>24000</td>
+				</tr>
+				<tr>
+					<td className={"border p-4"}>Демонтаж насосов управления</td>
+					<td className={"border"}>6</td>
+					<td className={"border"}>9000</td>
+					<td className={"border"}>7</td>
+					<td className={"border"}>10500</td>
+				</tr>
+				<tr>
+					<td className={"border p-4"}>Монтаж насосов управления</td>
+					<td className={"border"}>6</td>
+					<td className={"border"}>9000</td>
+					<td className={"border"}>7</td>
+					<td className={"border"}>10500</td>
+				</tr>
+				<tr>
+					<td className={"border p-3 font-bold"} colSpan={5}>КПП (неспаренная с ГТР)</td>
+				</tr>
+				<tr>
+					<td className={"border p-4"}>Диагностика КПП</td>
+					<td className={"border"}>2</td>
+					<td className={"border"}>3000</td>
+					<td className={"border"}>2</td>
+					<td className={"border"}>3000</td>
+				</tr>
+				<tr>
+					<td className={"border p-4"}>Демонтаж КПП</td>
+					<td className={"border"}>12</td>
+					<td className={"border"}>18000</td>
+					<td className={"border"}>16</td>
+					<td className={"border"}>24000</td>
+				</tr>
+				<tr>
+					<td className={"border p-4"}>Разборка, мойка и дефектовка КПП</td>
+					<td className={"border"}>12</td>
+					<td className={"border"}>18000</td>
+					<td className={"border"}>14</td>
+					<td className={"border"}>21000</td>
+				</tr>
+				<tr>
+					<td className={"border p-4"}>Сборка КПП</td>
+					<td className={"border"}>16</td>
+					<td className={"border"}>24000</td>
+					<td className={"border"}>18</td>
+					<td className={"border"}>27000</td>
+				</tr>
+				<tr>
+					<td className={"border p-4"}>Монтаж КПП</td>
+					<td className={"border"}>12</td>
+					<td className={"border"}>18000</td>
+					<td className={"border"}>16</td>
+					<td className={"border"}>24000</td>
+				</tr>
+				<tr>
+					<td className={"border p-4"}>Демонтаж насосов управления</td>
+					<td className={"border"}>6</td>
+					<td className={"border"}>9000</td>
+					<td className={"border"}>6</td>
+					<td className={"border"}>9000</td>
+				</tr>
+				<tr>
+					<td className={"border p-4"}>Монтаж насосов управления</td>
+					<td className={"border"}>6</td>
+					<td className={"border"}>9000</td>
+					<td className={"border"}>6</td>
+					<td className={"border"}>9000</td>
+				</tr>
+				<tr>
+					<td className={"border p-3 font-bold"} colSpan={5}>Гидросистема и рулевое управление</td>
+				</tr>
+				<tr>
+					<td className={"border p-4"}>Диагностика</td>
+					<td className={"border"}>4</td>
+					<td className={"border"}>6000</td>
+					<td className={"border"}>6</td>
+					<td className={"border"}>9000</td>
+				</tr>
+				<tr>
+					<td className={"border p-4"}>Демонтаж гидробака</td>
+					<td className={"border"}>3</td>
+					<td className={"border"}>4500</td>
+					<td className={"border"}>3</td>
+					<td className={"border"}>4500</td>
+				</tr>
+				<tr>
+					<td className={"border p-4"}>Демонтаж, разборка, мойка и дефектовка  распределителя  управления гидросистемы</td>
+					<td className={"border"}>8</td>
+					<td className={"border"}>12000</td>
+					<td className={"border"}>8</td>
+					<td className={"border"}>12000</td>
+				</tr>
+				<tr>
+					<td className={"border p-4"}>Разборка, мойка и дефектовка насосов управления</td>
+					<td className={"border"}>8</td>
+					<td className={"border"}>12000</td>
+					<td className={"border"}>8</td>
+					<td className={"border"}>12000</td>
+				</tr>
+				<tr>
+					<td className={"border p-4"}>Сборка насосов управления</td>
+					<td className={"border"}>8</td>
+					<td className={"border"}>12000</td>
+					<td className={"border"}>8</td>
+					<td className={"border"}>12000</td>
+				</tr>
+				<tr>
+					<td className={"border p-4"}>Монтаж гидробака</td>
+					<td className={"border"}>3</td>
+					<td className={"border"}>4500</td>
+					<td className={"border"}>3</td>
+					<td className={"border"}>4500</td>
+				</tr>
+				<tr>
+					<td className={"border p-4"}>Монтаж распределителей</td>
+					<td className={"border"}>4</td>
+					<td className={"border"}>6000</td>
+					<td className={"border"}>4</td>
+					<td className={"border"}>6000</td>
+				</tr>
+				<tr>
+					<td className={"border p-4"}>Замена гидроцилиндра управления (стреловой, ковшевой) 1 шт.</td>
+					<td className={"border"}>1.5</td>
+					<td className={"border"}>2250</td>
+					<td className={"border"}>2</td>
+					<td className={"border"}>3000</td>
+				</tr>
+				<tr>
+					<td className={"border p-4"}>Замена насос-дозатора управления рулевой системы</td>
+					<td className={"border"}>6</td>
+					<td className={"border"}>9000</td>
+					<td className={"border"}>8</td>
+					<td className={"border"}>12000</td>
+				</tr>
+				<tr>
+					<td className={"border p-4"}>Ремонт гидроцилиндра (стреловой, ковшевой)</td>
+					<td className={"border"}>7</td>
+					<td className={"border"}>10500</td>
+					<td className={"border"}>8</td>
+					<td className={"border"}>12000</td>
+				</tr>
+				<tr>
+					<td className={"border p-4"}>Замена рулевого гидроцилиндра</td>
+					<td className={"border"}>1</td>
+					<td className={"border"}>1500</td>
+					<td className={"border"}>1.5</td>
+					<td className={"border"}>2250</td>
+				</tr>
+				<tr>
+					<td className={"border p-4"}>Ремонт рулевого  гидроцилиндра</td>
+					<td className={"border"}>5</td>
+					<td className={"border"}>7500</td>
+					<td className={"border"}>6</td>
+					<td className={"border"}>9000</td>
+				</tr>
+				<tr>
+					<td className={"border p-4"}>Замена РВД (1шт)</td>
+					<td className={"border"}>0.5</td>
+					<td className={"border"}>750</td>
+					<td className={"border"}>0.5</td>
+					<td className={"border"}>750</td>
+				</tr>
+				<tr>
+					<td className={"border p-3 font-bold"} colSpan={5}>Тормозная система и пневмосистема</td>
+				</tr>
+				<tr>
+					<td className={"border p-4"}>Диагностика тормозной системы</td>
+					<td className={"border"}>3</td>
+					<td className={"border"}>4500</td>
+					<td className={"border"}>4</td>
+					<td className={"border"}>6000</td>
+				</tr>
+				<tr>
+					<td className={"border p-4"}>Замена суппорта (1шт.)</td>
+					<td className={"border"}>2</td>
+					<td className={"border"}>3000</td>
+					<td className={"border"}>3</td>
+					<td className={"border"}>4500</td>
+				</tr>
+				<tr>
+					<td className={"border p-4"}>Замена торм. колодок (1шт.)</td>
+					<td className={"border"}>2</td>
+					<td className={"border"}>3000</td>
+					<td className={"border"}>3</td>
+					<td className={"border"}>4500</td>
+				</tr>
+				<tr>
+					<td className={"border p-4"}>Замена (регулировка) стояночного тормоза</td>
+					<td className={"border"}>2</td>
+					<td className={"border"}>3000</td>
+					<td className={"border"}>4</td>
+					<td className={"border"}>6000</td>
+				</tr>
+				<tr>
+					<td className={"border p-4"}>Удаление воздуза из торм. системы</td>
+					<td className={"border"}>2</td>
+					<td className={"border"}>3000</td>
+					<td className={"border"}>5</td>
+					<td className={"border"}>7500</td>
+				</tr>
+				<tr>
+					<td className={"border p-4"}>Замена клапана сброса воздуха</td>
+					<td className={"border"}>1</td>
+					<td className={"border"}>1500</td>
+					<td className={"border"}>1.5</td>
+					<td className={"border"}>2250</td>
+				</tr>
+				<tr>
+					<td className={"border p-4"}>Проверка на герметичность пневмосистемы</td>
+					<td className={"border"}>2</td>
+					<td className={"border"}>3000</td>
+					<td className={"border"}>2</td>
+					<td className={"border"}>3000</td>
+				</tr>
+				<tr>
+					<td className={"border p-4"}>Замена компрессора</td>
+					<td className={"border"}>3</td>
+					<td className={"border"}>4500</td>
+					<td className={"border"}>4</td>
+					<td className={"border"}>6000</td>
+				</tr>
+				<tr>
+					<td className={"border p-3 font-bold"} colSpan={5}>Ходовая часть</td>
+				</tr>
+				<tr>
+					<td className={"border p-4"}>Замена колеса в сборе</td>
+					<td className={"border"}>1</td>
+					<td className={"border"}>1500</td>
+					<td className={"border"}>1</td>
+					<td className={"border"}>1500</td>
+				</tr>
+				<tr>
+					<td className={"border p-4"}>Замена моста в сборе</td>
+					<td className={"border"}>12</td>
+					<td className={"border"}>18000</td>
+					<td className={"border"}>12</td>
+					<td className={"border"}>18000</td>
+				</tr>
+				<tr>
+					<td className={"border p-4"}>Разборка, мойка и дефектовка бортового редуктора</td>
+					<td className={"border"}>4</td>
+					<td className={"border"}>6000</td>
+					<td className={"border"}>5</td>
+					<td className={"border"}>7500</td>
+				</tr>
+				<tr>
+					<td className={"border p-4"}>Замена (ремонт) главной передачи</td>
+					<td className={"border"}>8</td>
+					<td className={"border"}>12000</td>
+					<td className={"border"}>8</td>
+					<td className={"border"}>12000</td>
+				</tr>
+				<tr>
+					<td className={"border p-4"}>Сборка бортового редуктора</td>
+					<td className={"border"}>5</td>
+					<td className={"border"}>7500</td>
+					<td className={"border"}>6</td>
+					<td className={"border"}>9000</td>
+				</tr>
+				<tr>
+					<td className={"border p-4"}>Монтаж карданного вала</td>
+					<td className={"border"}>1</td>
+					<td className={"border"}>1500</td>
+					<td className={"border"}>1</td>
+					<td className={"border"}>1500</td>
+				</tr>
+				<tr>
+					<td className={"border p-4"}>Демонтаж карданного вала</td>
+					<td className={"border"}>1</td>
+					<td className={"border"}>1500</td>
+					<td className={"border"}>1</td>
+					<td className={"border"}>1500</td>
+				</tr>
+				<tr>
+					<td className={"border p-4"}>Замена крестовины (1 шт)</td>
+					<td className={"border"}>1</td>
+					<td className={"border"}>1500</td>
+					<td className={"border"}>1</td>
+					<td className={"border"}>1500</td>
+				</tr>
+				<tr>
+					<td className={"border p-3 font-bold"} colSpan={5}>Электрооборудование</td>
+				</tr>
+				<tr>
+					<td className={"border p-4"}>Диагностика электрообоудования</td>
+					<td className={"border"}>3</td>
+					<td className={"border"}>4500</td>
+					<td className={"border"}>3</td>
+					<td className={"border"}>4500</td>
+				</tr>
+				<tr>
+					<td className={"border p-4"}>Замена жгутов электропроводки</td>
+					<td className={"border"}>20</td>
+					<td className={"border"}>3000</td>
+					<td className={"border"}>24</td>
+					<td className={"border"}>36000</td>
+				</tr>
+				<tr>
+					<td className={"border p-4"}>Замена генератора/стартера</td>
+					<td className={"border"}>2</td>
+					<td className={"border"}>3000</td>
+					<td className={"border"}>2</td>
+					<td className={"border"}>3000</td>
+				</tr>
+				<tr>
+					<td className={"border p-4"}>Замена прибора/датчика</td>
+					<td className={"border"}>1</td>
+					<td className={"border"}>1500</td>
+					<td className={"border"}>1</td>
+					<td className={"border"}>1500</td>
+				</tr>
+				<tr>
+					<td className={"border p-4"}>Замена фар</td>
+					<td className={"border"}>3</td>
+					<td className={"border"}>4500</td>
+					<td className={"border"}>3</td>
+					<td className={"border"}>4500</td>
+				</tr>
+				<tr>
+					<td className={"border p-3 font-bold"} colSpan={5}>Прочее</td>
+				</tr>
+				<tr>
+					<td className={"border p-4"}>Замена втулок и пальцев сочленения рамы</td>
+					<td className={"border"}>16</td>
+					<td className={"border"}>24000</td>
+					<td className={"border"}>20</td>
+					<td className={"border"}>30000</td>
+				</tr>
+				<tr>
+					<td className={"border p-4"}>Замена пальцев и втулок оборудования</td>
+					<td className={"border"}>12</td>
+					<td className={"border"}>18000</td>
+					<td className={"border"}>16</td>
+					<td className={"border"}>24000</td>
+				</tr>
+				<tr>
+					<td className={"border p-4"}>Демонтаж кабины</td>
+					<td className={"border"}>4</td>
+					<td className={"border"}>6000</td>
+					<td className={"border"}>6</td>
+					<td className={"border"}>9000</td>
+				</tr>
+				<tr>
+					<td className={"border p-4"}>Монтаж кабины</td>
+					<td className={"border"}>4</td>
+					<td className={"border"}>6000</td>
+					<td className={"border"}>6</td>
+					<td className={"border"}>9000</td>
+				</tr>
+				<tr>
+					<td className={"border p-4"}>Демонтаж/монтаж ковша</td>
+					<td className={"border"}>1.5</td>
+					<td className={"border"}>2250</td>
+					<td className={"border"}>2</td>
+					<td className={"border"}>3000</td>
+				</tr>
+				<tr>
+					<td className={"border p-4"}>Установка сепаратора</td>
+					<td className={"border"}>6</td>
+					<td className={"border"}>9000</td>
+					<td className={"border"}>6</td>
+					<td className={"border"}>9000</td>
+				</tr>
+				<tr>
+					<td className={"border p-4"}>Установка электрического подогревателя</td>
+					<td className={"border"}>8</td>
+					<td className={"border"}>12000</td>
+					<td className={"border"}>8</td>
+					<td className={"border"}>12000</td>
+				</tr>
 			</tbody>
 		</table>
 
