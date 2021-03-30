c7eb27ad107b18b573f988a0bbfd89d7aeab2225
Dmitriy Konovalov <dimusig@gmail.com>
2021-03-30 23:22:11 +0300

    fix:  MarkDown to contetnFlow

--- components/sections/ContentFlow/ContentFlow.jsx
+++ components/sections/ContentFlow/ContentFlow.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import PropTypes from "prop-types";
+import ReactMarkdown from "react-markdown";
 import cx from "classnames";
 
 import Heading from "@/components/elements/Heading";
@@ -7,33 +8,36 @@ import Picture from "@/components/elements/Picture";
 
 export default function ContentFlow({ items }) {
 	return (
-		<div className={"content-flow"}>
-			{
-				items.map(({ heading, picture, content }, idx) => {
-					return (
-						<div key={idx} className={"content-flow__item"}>
-							<Heading heading={heading} />
-							<div
-								className={
-									cx(
-										"content-flow__body flex flex-column",
-										{ "md:flex-row": idx % 2 },
-										{ "md:flex-row-reverse": !(idx % 2) },
-									)
-								}
-							>
-								<div className={"content-flow__picture"}>
-									<Picture items={picture} />
-								</div>
-								<div className={"content-flow__content"}>
-									{/* TODO: NEED TO USE REACT MARKDOWN */}
-									{content}
+		<div className={"container"}>
+			<div className={"content-flow"}>
+				{
+					items.map(({ heading, picture, content }, idx) => {
+						return (
+							<div key={idx} className={"content-flow__item"}>
+								<Heading heading={heading} />
+								<div
+									className={
+										cx(
+											"content-flow__body flex flex-column",
+											{ "md:flex-row": idx % 2 },
+											{ "md:flex-row-reverse": !(idx % 2) },
+										)
+									}
+								>
+									<div className={"content-flow__picture"}>
+										<Picture items={picture} />
+									</div>
+									<div className={"content-flow__content"}>
+										<ReactMarkdown>
+											{content}
+										</ReactMarkdown>
+									</div>
 								</div>
 							</div>
-						</div>
-					);
-				})
-			}
+						);
+					})
+				}
+			</div>
 		</div>
 	);
 }
