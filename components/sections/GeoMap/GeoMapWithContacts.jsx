ccb9c131b69e090392eb047e300c600c705d2428
Dmitriy Konovalov <dimusig@gmail.com>
2021-04-01 23:47:13 +0300

    fix: feedback form markup

--- components/sections/GeoMap/GeoMapWithContacts.jsx
+++ components/sections/GeoMap/GeoMapWithContacts.jsx
@@ -12,13 +12,15 @@ export default function MapSection() {
 			<div className={"w-full h-60 md:h-96 relative"} style={{ minHeight: "35rem" }}>
 				<GeoMap />
 				<div className={"hidden md:flex h-full w-3/12 md:absolute top-0 right-20 items-center"}>
-					<div className={"h-5/6 w-full bg-gray-100 border border-gray-300 p-4 rounded-md flex flex-col items-stretch justify-between"}>
-						<div className={"text-center text-black mb-4"}>
-							<h2 className={"mb-4 uppercase"}>Заказать звонок</h2>
+					<div className={"h-5/6 w-full bg-gray-100 border border-gray-300 rounded-md flex flex-col items-stretch justify-between"}>
+						<div className={"border-b p-4 text-center"}>
+							<h2 className={"text-3xl font-bold mb-4"}>Заказать звонок</h2>
 							<p>Оставьте ваши контактные данные и мы с вами свяжемся</p>
 						</div>
-						<FeedbackForm />
-						<div className={"flex mt-4"}>
+						<div className={"px-4"}>
+							<FeedbackForm />
+						</div>
+						<div className={"flex p-4 border-t"}>
 							<div className={"flex-1"}>
 								<Button type={"primary"} text={"Заказать"} />
 							</div>
