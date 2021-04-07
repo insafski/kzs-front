680a674a23e6323c0945763687e2adc03456f671
Dmitriy Konovalov <dimusig@gmail.com>
2021-04-07 23:41:40 +0300

    fix: obect-fit news

--- components/elements/ListElement/News/News.jsx
+++ components/elements/ListElement/News/News.jsx
@@ -16,7 +16,7 @@ export default function News({ heading, publishedAt, picture, slug }) {
 			<div className={"news-item__picture mb-2 h-80"}>
 				<Link href={`/novosti/${slug}`}>
 					<a>
-						<Picture items={picture} className={"object-none"}/>
+						<Picture items={picture} className={"object-cover"}/>
 					</a>
 				</Link>
 			</div>
